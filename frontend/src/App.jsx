ef79d835b5bf1fe735581f878cff91fb191a75f7
anshu <anshu.stackcraft@gmail.com>
2026-02-03 20:45:36 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,14 @@ import Home from "./pages/HomePage";
 import Nav from "./components/Nav";
 import GetInTouch from "./components/GetInTouch";
 import DesignSection from "./components/DesignSection";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
+// import Projects from "./pages/Projects";
+// import About from "./pages/About";
 import Footer from "./components/Footer";
 import Logout from "./accounts/Logout";
 import Login from "./accounts/Login";
 import Register from "./accounts/Register";
-import HireMe from "./pages/HireMe";
-import Resume from "./pages/Resume";
+// import HireMe from "./pages/HireMe";
+// import Resume from "./pages/Resume";
 import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
@@ -32,10 +32,10 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/getintuch" element={<GetInTouch />} />
         <Route path="/design" element={<DesignSection />} />
-        <Route path="/projects" element={<Projects />} />
+        {/* <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/hireme" element={<HireMe />} />
-        <Route path="/resume" element={<Resume />} />
+        <Route path="/resume" element={<Resume />} /> */}
       </Routes>
 
       <Footer />
---
+++ frontend/src/PriviterRoute.jsx
@@ -0,0 +1,27 @@
+// import { Routes, Route, useLocation } from "react-router-dom";
+// import { useEffect } from "react";
+// import Projects from "./pages/Projects";
+// import About from "./pages/About";
+// import HireMe from "./pages/HireMe";
+// import Resume from "./pages/Resume";
+
+// function App() {
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [location.pathname]);
+
+//   return (
+
+//       <Routes>       
+//         <Route path="/projects" element={<Projects />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/hireme" element={<HireMe />} />
+//         <Route path="/resume" element={<Resume />} />
+//       </Routes>
+
+//   );
+// }
+
+// export default App;
